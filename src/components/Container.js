8c6a718eedf65515d764c573e23a2e45ae2534fd
Stefan <stefanguzun2005@gmail.com>
2022-06-17 01:27:04 +0300

    i made the 'details' component with styled-components

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,4 +1,4 @@
-import { ContMain, SideBar, Details, H4, H5, Para, Detailssecond } from "./Container.style"
+import { InputLonger, InputCont, Header, ContMain, SideBar, Details,H4, H3, H5, Para, Detailssecond, Link, Footer, Button, Simplediv, Input } from "./Container.style"
 
 const Container = () => {
     return (
@@ -6,9 +6,28 @@ const Container = () => {
             <SideBar></SideBar>
             <Details>
                 <Detailssecond>
-                    <H5>STEP 1 OF 3</H5>
-                    <H4>Contact Details</H4>
-                    <Para>welcome to United Properties, we are glad to see you! Let's get started by  letting us know a little</Para>
+                    <Header>
+                        <H5>STEP 1 OF 3</H5>
+                        <Para>Lost or Have Troubles?<Link> Get Help  → </Link></Para>
+                    </Header>
+                        <H3>Contact Details</H3>
+                        <Para>welcome to United Properties, we are glad to see you! Let's get started by  letting us know a little</Para>
+                        <InputCont>
+                            <Input></Input>
+                            <Input></Input>
+                            <InputLonger></InputLonger>
+                            <InputLonger></InputLonger>
+                        </InputCont>
+                        <H4>Privacy Policy</H4>
+                        <Para>we know you care about how personal information is used and shared, so we take your privacy seriously</Para>
+                        <Para color="#35a1ee">Expand privacy policy →</Para>
+                    <Footer>
+                    <Para color="#3988dd">← Back to the previous</Para> 
+                        <Simplediv>
+                            <Button color="#3988dd" backgroundColor="#edf7fd">Skip for now</Button> 
+                            <Button color="white" backgroundColor="#35a1ee">Next stop→</Button>
+                        </Simplediv>
+                    </Footer>
                 </Detailssecond>
             </Details>
         </ContMain>
--- src/components/Container.style.js
+++ src/components/Container.style.js
@@ -1,5 +1,11 @@
 import styled from "styled-components"
 
+export const Simplediv = styled.div`
+`
+export const InputCont = styled.div`
+width: 100%;
+height: 150px;
+`
 export const ContMain = styled.div`
 display: flex;
 border-radius: 5px;
@@ -15,29 +21,102 @@ background-color: black;
 export const Details = styled.div`
 display: flex;
 justify-content: center;
-width: 600px;
+width: 700px;
 height: 600px;
 background-color: #ffffff;
 border-top-right-radius: 5px;
 border-bottom-right-radius: 5px;
 `
 export const Detailssecond = styled.div`
+display: flex;
+flex-direction: column;
 width: 450px;
 height: 600px;
 background-color: #ffffff;
 border-top-right-radius: 5px;
 border-bottom-right-radius: 5px;
+position: relative;
+left: -20px;
 `
-export const H4 = styled.h4`
+export const Header = styled.div`
 display: flex;
+justify-content: space-between;
+width: 100%;
+height: 80px;
+`
+export const Footer = styled.div`
+justify-content: space-between;
+position: relative;
+bottom: -50px;
+align-self: flex-end;
+display: flex;
+width: 100%;
+height: 50px;
+`
+export const H3 = styled.h3`
+display: flex;
+margin-bottom: 0;
+font-family: 'Roboto', sans-serif;
 `
 export const H5 = styled.h5`
+font-family: 'Roboto', sans-serif;
 display: flex;
 left: 10px;
 color: gray;`
-export const Para = styled.p`
+export const H4 = styled.h4`
+font-family: 'Roboto', sans-serif;
 display: flex;
-justify-content: flex-start;
+`
+export const Para = styled.p`
+font-family: 'Roboto', sans-serif;
+display: flex;
 color: gray;
 font-size: small;
+text-align: left;
+margin-top: 10px;
+color: ${(props) => props.color};
+`
+export const Link = styled.a`
+font-family: 'Roboto', sans-serif;
+color: #3988dd;
+`
+export const Button = styled.button`
+font-family: 'Roboto', sans-serif;
+color: ${(props) => props.color};
+text-align: center;
+border: none;
+width: 110px;
+height: 35px;
+border-radius: 3px;
+background-color: ${(props) => props.backgroundColor};
+margin-right: 10px;
+&:active{
+    box-shadow: rgba(136, 165, 191, 0.48) 6px 2px 16px 0px, rgba(255, 255, 255, 0.8) -6px -2px 16px 0px;
+}
+`
+export const Input = styled.input`
+margin: 10px;
+width: 200px;
+height: 20px;
+  border-top: hidden;
+  border-right: hidden;
+  border-left: hidden;
+  border-bottom: lightgrey solid 2px;
+&:focus{
+    outline: none;
+    border-bottom: #3988dd solid 2px;
+}
+`
+export const InputLonger = styled.input`
+margin: 10px;
+width: 420px;
+height: 20px;
+  border-top: hidden;
+  border-right: hidden;
+  border-left: hidden;
+  border-bottom: lightgrey solid 2px;
+&:focus{
+    outline: none;
+    border-bottom: #3988dd solid 2px;
+}
 `
