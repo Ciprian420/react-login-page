502e657629473d30bd0c98925cf74223426f5358
Iulian <iulian.gonta13@gmail.com>
2022-06-19 01:21:43 +0300

    Added the progress bar for the money input

---
+++ src/Components/MoneyProgressBar/MoneyProgressBarStyle.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+
+export const Square = styled.div`
+display: flex;
+width: 23px;
+height: 23px;
+background-color: #35a1ee;
+border-radius: 2px;
+flex-direction: column;
+justify-content: center;
+margin-left: -8px;
+`
+
+
+export const OrizontalLine = styled.div`
+display: flex;
+align-items: center;
+flex-direction: row-reverse;
+width: 160px;
+height: 5px;
+background-color: #35a1ee
+`
+
+export const VerticalLine = styled.div`
+border-radius: 1px;
+width: 5px;
+height: 22px;
+background-color: #35a1ee
+`
+
+export const Container = styled.div`
+display: flex;
+align-items: baseline;
+`
+
+export const Price = styled.span`
+position: relative;
+top: 50px;
+left: -28px;
+color: #35A1EEFF;
+font-weight: bolder;`
