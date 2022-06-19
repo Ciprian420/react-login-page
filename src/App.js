3a8cac9d3cd64018752f8b98af51d901ac091688
stefan <stefanguzun2005@gmail.com>
2022-06-20 00:57:48 +0300

    i created the preferences component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Details from './Components/Details/Details';
 import Container2 from "./Components/Investment/Investment";
+import Preferences from './Components/Preferences/Preferences';
 
 function App() {
   return (
    <div className="App">
-    <Container2></Container2>
+    <Preferences></Preferences>
    </div>
   )
 }
