c859510410ef25caf12fe13c0c7438e4eca101c9
DoomPin <pynzaru.dima@mail.ru>
2022-06-20 10:06:07 +0300

    Investment plan 100%

--- src/Components/MoneyProgressBar/MoneyProgressBarStyle.js
+++ src/Components/MoneyProgressBar/MoneyProgressBarStyle.js
@@ -2,30 +2,29 @@ import styled from "styled-components";
 
 
 export const Square = styled.div`
-display: flex;
-width: 23px;
-height: 23px;
+margin-top: 10px;
+width: 20px;
+height: 20px;
 background-color: #35a1ee;
 border-radius: 2px;
-flex-direction: column;
-justify-content: center;
 margin-left: -8px;
 `
 
 
 export const OrizontalLine = styled.div`
+margin-top: 10px;
 display: flex;
 align-items: center;
 flex-direction: row-reverse;
 width: 87px;
-height: 5px;
+height: 4px;
 background-color: #35a1ee
 `
 
 export const VerticalLine = styled.div`
 border-radius: 1px;
-width: 5px;
-height: 22px;
+width: 4px;
+height: 15px;
 background-color: #35a1ee
 `
 
