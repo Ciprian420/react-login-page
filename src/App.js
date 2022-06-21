a27bc3ac7f301df9107cc3c865f57fafcec7f808
Ciprian <moraruciprian2005@gmail.com>
2022-06-21 17:34:25 +0300

    i added firebase

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import Details from './Components/Details/Details';
 import Container2 from "./Components/Investment/Investment";
 import Preferences from './Components/Preferences/Preferences';
+import Investment from "./Components/Investment/Investment";
 
 function App() {
   return (
    <div className="App">
-    <Container2></Container2>
+    <Investment/>
    </div>
   )
 }
